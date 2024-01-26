7c0beb49a9f0ed63dc1fddebb311ab0a30fd2dac
mooojn <moontariq7867@gmail.com>
2024-01-26 08:50:44 +0500

    class for enemy's character info

---
+++ PD-Lab/Week-2/enemy-class.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_conv
+{
+    internal class enemy_class
+    {
+    }
+}
