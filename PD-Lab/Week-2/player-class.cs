2e6e3c0466b796c9cd993e905e83c727ea59e0cd
mooojn <moontariq7867@gmail.com>
2024-01-26 09:03:32 +0500

    parameterized construct

--- PD-Lab/Week-2/player-class.cs
+++ PD-Lab/Week-2/player-class.cs
@@ -8,5 +8,16 @@ namespace game_conv
 {
     internal class player_class
     {
+        // data strut for player
+        public char player_symbol;
+        public int player_x;
+        public int player_y;
+        // initializing vals
+        public player_class(char symbol, int x_axis, int y_axis)
+        {
+            player_symbol = symbol;
+            player_x = x_axis;
+            player_y = y_axis;
+        }
     }
 }
