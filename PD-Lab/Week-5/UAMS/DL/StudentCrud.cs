7eadbd75b8b1c4259a6c5f71eddb21acfcf78c72
mooojn <moontariq7867@gmail.com>
2024-02-24 17:22:10 +0500

    data loading and storing from file

--- PD-Lab/Week-5/UAMS/DL/StudentCrud.cs
+++ PD-Lab/Week-5/UAMS/DL/StudentCrud.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using chlng4_new.DL;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -10,6 +12,49 @@ namespace chlng4_new
     internal class StudentCrud
     {
         public static List<Student> Students = new List<Student>();
+        public static void LoadStudentsFrom(string path)
+        {
+            StreamReader F = new StreamReader(path);
+            string record;
+            if (File.Exists(path))
+            {
+                while ((record = F.ReadLine()) != null)
+                {
+                    string[] data = record.Split(',');
+                    string name = data[0];
+                    int age = int.Parse(data[1]);
+                    double fsc = double.Parse(data[2]);
+                    double ecat = double.Parse(data[3]);
+
+                    string[] pref = data[4].Split(';');
+
+                    List<Degree> Pref = new List<Degree>();
+
+                    for (int i = 0; i < pref.Length; ++i)
+                    {
+                        Degree D = DegreeCrud.IsDegreeExist(pref[i]); 
+                        if (D != null)
+                        {
+                            Pref.Add(D);
+                        }
+                    }
+                    Student S = new Student(name, age, fsc, ecat, Pref);
+                    
+                    Students.Add(S);
+                }
+                F.Close();
+            }
+        }
+        public static void StoreStudentsTo(string path)
+        {
+            StreamWriter F = new StreamWriter(path);
+            foreach (Student S in Students)
+            {
+                string preferences = string.Join(";", S.Preferences.Select(o => o.Title));     // getting all the titles of degrees
+                F.WriteLine($"{S.Name},{S.Age},{S.FscMarks},{S.EcatMarks},{preferences}");
+            }
+            F.Close();
+        }
         public static void AddStudent(Student NewStudent)
         {
             Students.Add(NewStudent);
