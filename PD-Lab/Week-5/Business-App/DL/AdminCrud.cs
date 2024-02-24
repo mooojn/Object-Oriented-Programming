bc9942024ab021ed56e11c05f89a242a7f580d73
mooojn <moontariq7867@gmail.com>
2024-02-24 21:01:09 +0500

    Crud

---
+++ PD-Lab/Week-5/Business-App/DL/AdminCrud.cs
@@ -0,0 +1,24 @@
+﻿using buscompe_conv.UI;
+using chlg3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chlg3
+{
+    internal class AdminCrud
+    {
+        public static bool Validate(string pass, string admin_pass)
+        {
+            if (pass == admin_pass)
+            {
+                UtilUi.success("You are signed in as admin...");
+                return true;
+            }
+            UtilUi.error("Incorrect Password");
+            return false;
+        }
+    }
+}
