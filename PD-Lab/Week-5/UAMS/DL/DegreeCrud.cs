1a14cab153a73ebb6e7a27cd58280b1ec477d7d2
mooojn <moontariq7867@gmail.com>
2024-02-24 17:22:14 +0500

    data loading and storing from file

--- PD-Lab/Week-5/UAMS/DL/DegreeCrud.cs
+++ PD-Lab/Week-5/UAMS/DL/DegreeCrud.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,59 @@ namespace chlng4_new.DL
         public static void RegisteredStd(int index)
         {
             Degrees[index].Registered();
+        }
+        public static void LoaDegreesFrom(string path)
+        {
+            StreamReader F = new StreamReader(path);
+            string record;
+            if (File.Exists(path))
+            {
+                while ((record = F.ReadLine()) != null)
+                {
+                    string[] data = record.Split(',');
+                    string name = data[0];
+                    float duration = float.Parse(data[1]);
+                    int seats = int.Parse(data[2]);
+                    
+                    string[] subjectsCode = data[3].Split(';');
+                    
+                    List<Subject> Sub = new List<Subject>();
+                    
+                    for(int i = 0; i < subjectsCode.Length; ++i)
+                    {
+                        Subject S = SubjectCrud.IsSubjectExist(subjectsCode[i]);
+                        if (S != null)
+                        {
+                            Sub.Add(S);
+                        }
+                    }
+                    Degree degree = new Degree(name, duration, seats, Sub);
+                    
+                    Degrees.Add(degree);
+                }
+                F.Close();
+            }
+        }
+        public static void StoreDegreesTo(string path)
+        {
+            StreamWriter F = new StreamWriter(path);
+            foreach (Degree D in Degrees)
+            {
+                string subjectCodes = string.Join(";", D.Subjects.Select(o => o.Code));     // getting all the codes of subjects
+                F.WriteLine($"{D.Title},{D.Duration},{D.Seats},{subjectCodes}");
+            }
+            F.Close();
+        }
+        public static Degree IsDegreeExist(string name)
+        {
+            foreach(Degree D in Degrees)
+            {
+                if (D.Title == name)
+                {
+                    return D;
+                }
+            }
+            return null;
         }
     }
 }
