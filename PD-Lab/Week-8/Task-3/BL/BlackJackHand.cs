127034d059e96bda49ac068d07b70fc25707d24a
mooojn <moontariq7867@gmail.com>
2024-03-22 21:44:05 +0500

    task3

---
+++ PD-Lab/Week-8/Task-3/BL/BlackJackHand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class BlackJackHand : Hand
+    {
+        public int getBlackJackValue()
+        {
+            int value = 0; 
+            bool ace = false;
+            foreach (Card c in cards) {
+                int cardValue = c.getValue();
+                if (cardValue > 10) {
+                    cardValue = 10;
+                }
+                else if (cardValue == 1) {
+                    ace = true;
+                }
+                value += cardValue;
+            }
+            if (ace && value + 11 <= 21) {
+                value += 11;
+            }
+            return value;
+        }
+    }
+}
